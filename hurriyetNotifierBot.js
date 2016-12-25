6644b0c17ae5d2c755f74bf0706faa8278d7d4bc
birkan.kolcu <birkan.kolcu@ozu.edu.tr>
2016-12-25 01:10:44 -0500

    added package.json, initialized the project as npm

--- hurriyetNotifierBot.js
+++ hurriyetNotifierBot.js
@@ -1,41 +1,80 @@
 const appConfig = {
   hurriyetApiKey: "3d6c4857fdff4fa58051493e20da9050",
-  fetchLimit: 10,
+  fetchLimit: 5,
   fetchIntervalInMinutes: 1,
   newsIdCollection: "newsIdCollection", 
   newsDataCollection: "newsDataCollection",
   
-  keywordListName: "keywordList",             //chatter
+  keywordListName:      "keywordList",        //chatter
   notificationListName: "notificationLists",  //chatter
+  userCollectionName:   "userCollection",     //chatter
   
   
   mongoUrl: "mongodb://umut:yoyo123@ds145128.mlab.com:45128/hurriyet-hao",
-  telegramApiKey: "315275698:AAF6ED0BY4P2n8PiT0FsddkLREaXLpbxFS0"
+  telegramApiKey: "316839828:AAGQ_zU14CB-iQ36X10e4bH28v20MVkT_kc"
 };
 
 var chatterModule  = require("./lib/chatterModule");
 var hurriyetModule = require("./lib/hurriyetModule");
 
+let latestNewsRequestReceivedCallback = function(userId) {
+  var maxNewsCount = 3;
+  hurriyetModule.getLatestNews(maxNewsCount, function(err, result){
+    if(!err) {
+      let message = "Gündemden "+result.length+" haber:";
+      chatterModule.sendMessageToUser(userId,message);  
+      result.forEach(function(newsData){
+        setTimeout(function(){
+          chatterModule.sendMessageToUser(userId,"\n " + newsData.Url);
+        },100);
+      });
+    } else {
+      chatterModule.sendMessageToUser(userId,"Oops, sistemde bir hata oluştu :(");
+    }
+  })
+};
+
 function randomInt (low, high) {
     return Math.floor(Math.random() * (high - low) + low);
+}
+
+function notifyAllRelatedUsers(keyword, url){
+  chatterModule.getNotificationList(keyword, function(err, notificationList){
+        if(!err) {
+          var message = keyword + " hakkinda yeni bir haber buldum. \n" + url;
+          notificationList.forEach(function(userId) {
+            var delayMillis = randomInt(500, 5000);
+            setTimeout(function() {
+              chatterModule.sendMessageToUser(userId, message);
+            }, delayMillis);
+          });
+        } else {
+          throw err;
+        }
+      });
 }
 
 var newsAddedCallback = function(err, result){
   console.log("hurriyetNotifierBot - newsAddedCallback")
   var newsData          = result.newsData;
-  var extractedKeywords = result.extractedKeywords;
-  console.log("HERE EXTRACTED KEYWORDS: "+JSON.stringify(extractedKeywords));
+  var newsDescription   = newsData.Description;
+  
+  //var extractedKeywords = result.extractedKeywords;
+
   var userKeywords = chatterModule.getUserKeywords();
+  userKeywords.forEach(function(userKeyword) {
+    if(newsDescription.includes(userKeyword)) {
+      notifyAllRelatedUsers(userKeyword, newsData.Url);
+    }
+  });
+  /*
   extractedKeywords.forEach(function(extractedKeyword){
     if (userKeywords.indexOf(extractedKeyword) > -1) {
-      console.log("hurriyetNotifierBot - getNotificationList 1")
       chatterModule.getNotificationList(extractedKeyword, function(err, notificationList){
-        console.log("hurriyetNotifierBot - getNotificationList 2")
         if(!err) {
           var message = extractedKeyword + " hakkinda yeni bir haber buldum. \n" + newsData.Url;
           notificationList.forEach(function(userId) {
             var delayMillis = randomInt(500, 5000);
-            console.log("YEYAAAAAAAAA!!!");
             setTimeout(function() {
               chatterModule.sendMessageToUser(userId, message);
             }, delayMillis);
@@ -46,14 +85,16 @@ var newsAddedCallback = function(err, result){
       });
     }
   });
+  */
 };
 
-chatterModule.initializeModule(appConfig.telegramApiKey, appConfig.mongoUrl, appConfig.notificationListName, appConfig.keywordListName ,function(err, result) {
+chatterModule.initializeModule(appConfig.telegramApiKey, appConfig.mongoUrl, appConfig.notificationListName, appConfig.keywordListName, appConfig.userCollectionName, function(err, result) {
   if(!err) {
     hurriyetModule.initializeModule(appConfig.mongoUrl,appConfig.hurriyetApiKey, appConfig.fetchIntervalInMinutes, appConfig.fetchLimit, appConfig.newsIdCollection, appConfig.newsDataCollection, function(err, result) {
       if(!err) {
         if(newsAddedCallback) {
           hurriyetModule.setNewsAddedCallback(newsAddedCallback);
+          chatterModule.setLatestNewsRequestReceivedCallback(latestNewsRequestReceivedCallback);
         } else {
           throw new Error("DAFFFFUQ");
         }
